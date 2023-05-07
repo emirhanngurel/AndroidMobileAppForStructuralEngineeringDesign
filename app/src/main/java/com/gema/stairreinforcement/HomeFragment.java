b41fbc8242701aeef48a49369135d957c7171e04
Emirhan Gürel <grlemirhan@outlook.com>
2023-05-07 16:45:33 +0300

    exception handling

--- app/src/main/java/com/gema/stairreinforcement/HomeFragment.java
+++ app/src/main/java/com/gema/stairreinforcement/HomeFragment.java
@@ -36,6 +36,7 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater,container,false);
         View view = binding.getRoot();
 
+
         Button calculateButton = (Button) binding.calculate;
         calculateButton.setOnClickListener(v -> {
 
@@ -47,29 +48,63 @@ public class HomeFragment extends Fragment {
             }
 
 
-            int numOfSteps = Integer.parseInt(binding.numOfSteps.getText().toString());
-            int rise = Integer.parseInt(binding.rise.getText().toString());
-            int runOfStep = Integer.parseInt(binding.run.getText().toString());
-            int widthOfStair = Integer.parseInt(binding.width.getText().toString());
-            int thickness = Integer.parseInt(binding.thickness.getText().toString());
+            int numOfSteps;
+            int rise;
+            int runOfStep;
+            int widthOfStair;
+            int thickness;
 
-            int tru = runOfStep * numOfSteps; // Total Run
-            int tri = rise * numOfSteps; // Total Rise
-            int amount = rise / runOfStep;
+
+            try{
+                numOfSteps = Integer.parseInt(binding.numOfSteps.getText().toString());
+                rise = Integer.parseInt(binding.rise.getText().toString());
+                runOfStep = Integer.parseInt(binding.run.getText().toString());
+                widthOfStair = Integer.parseInt(binding.width.getText().toString());
+                thickness = Integer.parseInt(binding.thickness.getText().toString());
+
+            }catch(NumberFormatException ex){
+                binding.numOfSteps.setError("Please enter a value");
+                binding.rise.setError("Please enter a value");
+                binding.run.setError("Please enter a value");
+                binding.width.setError("Please enter a value");
+                binding.thickness.setError("Please enter a value");
+                numOfSteps = 0;
+                rise = 0;
+                runOfStep = 0;
+                widthOfStair = 0;
+                thickness = 0;
+            }
+
+            int tru;
+            int tri;
+            int amount;
+
+            try{
+                tru = runOfStep * numOfSteps; // Total Run
+                tri = tri = rise * numOfSteps; // Total Rise
+                amount = rise / runOfStep;
+            }catch(ArithmeticException ae){
+                tru = 0;
+                tri = 0;
+                amount = 0;
+            }
+
             double rad = Math.atan(amount);
             double ss = rad * 180/Math.PI; // Stair slope
             double meter;
             double yard;
             double feet;
+            double total;
+            double cal;
 
             if(numOfSteps == 1){
-                double cal = rise*runOfStep*widthOfStair; // mm3
+                cal = rise*runOfStep*widthOfStair; // mm3
                 feet = cal * 0.0000000353146667214886; // ft3
                 yard = cal* 0.00000000130795061931439; //yd3
                 meter = cal * 0.000000001; //m3
             }
             else{
-                double total = ((rise*runOfStep*widthOfStair)/2 + Math.sqrt(rise*rise + runOfStep*runOfStep) * widthOfStair * thickness)*(numOfSteps-1)+rise*runOfStep*widthOfStair;
+                total = ((rise*runOfStep*widthOfStair)/2 + Math.sqrt(rise*rise + runOfStep*runOfStep) * widthOfStair * thickness)*(numOfSteps-1)+rise*runOfStep*widthOfStair;
                 feet = total * 0.0000000353146667214886; // ft3;
                 yard = total * 0.00000000130795061931439; //yd3
                 meter = total * 0.000000001; //m3
